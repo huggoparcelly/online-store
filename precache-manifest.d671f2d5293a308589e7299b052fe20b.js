2f8ff5682a10229b62f0942db4d3262727a97dd8
Huggo Parcelly <h.parcelly@gmail.com>
2021-10-04 19:26:51 -0300

    Updates

---
+++ precache-manifest.d671f2d5293a308589e7299b052fe20b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b52cd41033fda62c92f9d0d714ed772",
+    "url": "/online-store/index.html"
+  },
+  {
+    "revision": "e8a54c246f685257b141",
+    "url": "/online-store/static/css/main.f29b852f.chunk.css"
+  },
+  {
+    "revision": "e9c58a2c62538f40b1ac",
+    "url": "/online-store/static/js/2.e8698b00.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/online-store/static/js/2.e8698b00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a54c246f685257b141",
+    "url": "/online-store/static/js/main.d7e006e5.chunk.js"
+  },
+  {
+    "revision": "ff4896c2419cfc1775c2",
+    "url": "/online-store/static/js/runtime-main.a451c0e5.js"
+  }
+]);
